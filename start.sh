7affa8b9fa8c6f8532260cc0d6469c10b6bd5188
Hans Becker <hansbecker91@gmail.com>
2016-04-25 17:21:22 +0200

    start.sh now searches for Eclipse and Gradle jar start.sh no longer uses JAVA_HOME

--- start.sh
+++ start.sh
@@ -1,7 +1,23 @@
-$JAVA_HOME/bin/java -jar dist/services-python.jar \
-    -t -p -o -c \
-    -address tcp://* \
-    -registration tcp://*:5004 \
-    -configuration tcp://*:5007 \
-    -resources 5052 \
-    -dyndeps tcp://*:5009
+#!/usr/bin/env bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+eclipse_jar="$DIR/dist/services-python.jar"
+gradle_jar="$DIR/build/libs/services-python.jar"
+
+if [ -f "$eclipse_jar" ]; then
+    jar="$eclipse_jar"
+elif [ -f "$gradle_jar" ]; then
+    jar="$gradle_jar"
+else
+    printf "No jar found. Please build the project first.\n" >&2
+    exit 99
+fi
+
+java -jar "$jar" \
+     -t -p -o -c \
+     -address tcp://* \
+     -registration tcp://*:5004 \
+     -configuration tcp://*:5007 \
+     -resources 5052 \
+     -dyndeps tcp://*:5009
