035385a7b6af50e9d8203baaf5f0220ded5dd3c0
Hans Becker <hansbecker91@gmail.com>
2016-06-14 12:18:20 +0200

    Added Google Java Formatter to build script Reformatted all Java files according to Google StyleGuide

--- src/main/java/monto/service/python/PythonCodeCompletion.java
+++ src/main/java/monto/service/python/PythonCodeCompletion.java
@@ -1,5 +1,10 @@
 package monto.service.python;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
 import monto.service.MontoService;
 import monto.service.ZMQConfiguration;
 import monto.service.ast.AST;
@@ -16,196 +21,187 @@ import monto.service.request.Request;
 import monto.service.source.SourceMessage;
 import monto.service.types.Languages;
 
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.TreeSet;
-import java.util.stream.Collectors;
-
-
 public class PythonCodeCompletion extends MontoService {
 
-    public PythonCodeCompletion(ZMQConfiguration zmqConfig) {
-        super(zmqConfig,
-                PythonServices.PYTHON_CODE_COMPLETION,
-                "Code Completion",
-                "A code completion service for Python",
-                Languages.PYTHON,
-                Products.COMPLETIONS,
-                options(),
-                dependencies(new SourceDependency(Languages.PYTHON),
-                        new ProductDependency(PythonServices.PYTHON_PARSER, Products.AST, Languages.PYTHON))
-        );
-    }
+  public PythonCodeCompletion(ZMQConfiguration zmqConfig) {
+    super(
+        zmqConfig,
+        PythonServices.PYTHON_CODE_COMPLETION,
+        "Code Completion",
+        "A code completion service for Python",
+        Languages.PYTHON,
+        Products.COMPLETIONS,
+        options(),
+        dependencies(
+            new SourceDependency(Languages.PYTHON),
+            new ProductDependency(PythonServices.PYTHON_PARSER, Products.AST, Languages.PYTHON)));
+  }
 
+  @Override
+  public void onRequest(Request request) throws Exception {
+    SourceMessage version =
+        request
+            .getSourceMessage()
+            .orElseThrow(() -> new IllegalArgumentException("No version message in request"));
+    ProductMessage ast =
+        request
+            .getProductMessage(Products.AST, Languages.PYTHON)
+            .orElseThrow(() -> new IllegalArgumentException("No AST message in request"));
+
+    AST root = GsonMonto.fromJson(ast, AST.class);
+    List<Completion> allcompletions = allCompletions(version.getContents(), root);
+
+    List<Completion> relevant =
+        allcompletions
+            .stream()
+            .map(
+                comp
+                    -> new Completion(
+                        comp.getDescription() + ": " + comp.getReplacement(),
+                        comp.getReplacement(),
+                        0,
+                        comp.getIcon()))
+            .collect(Collectors.toList());
+
+    sendProductMessage(
+        version.getId(),
+        version.getSource(),
+        Products.COMPLETIONS,
+        Languages.PYTHON,
+        GsonMonto.toJsonTree(relevant));
+  }
+
+  private List<Completion> allCompletions(String contents, AST root) {
+    AllCompletions completionVisitor = new AllCompletions(contents);
+    root.accept(completionVisitor);
+    return completionVisitor.getCompletions();
+  }
+
+  private class AllCompletions implements ASTVisitor {
+
+    private TreeSet<String> variableNamesAlreadyOutlined = new TreeSet<String>();
+    private List<Completion> completions = new ArrayList<>();
+    private String content;
+
+    public AllCompletions(String content) {
+      this.content = content;
+    }
 
     @Override
-    public void onRequest(Request request) throws Exception {
-        SourceMessage version = request.getSourceMessage()
-                .orElseThrow(() -> new IllegalArgumentException("No version message in request"));
-        ProductMessage ast = request.getProductMessage(Products.AST, Languages.PYTHON)
-                .orElseThrow(() -> new IllegalArgumentException("No AST message in request"));
+    public void visit(NonTerminal node) {
 
-        AST root = GsonMonto.fromJson(ast, AST.class);
-        List<Completion> allcompletions = allCompletions(version.getContents(), root);
+      switch (node.getName()) {
+        case "funcdef":
+          addFuncToConverted(node, "def", getResource("method-public.png"));
+          break;
 
-        List<Completion> relevant =
-                allcompletions
-                        .stream()
-                        .map(comp -> new Completion(
-                                comp.getDescription() + ": " + comp.getReplacement(),
-                                comp.getReplacement(),
-                                0,
-                                comp.getIcon()))
-                        .collect(Collectors.toList());
+        case "classdef":
+          classToConverted(node, "class", getResource("class-public.png"));
+          break;
 
-        sendProductMessage(
-                version.getId(),
-                version.getSource(),
-                Products.COMPLETIONS,
-                Languages.PYTHON,
-                GsonMonto.toJsonTree(relevant));
+        case "global_stmt":
+          structureDeclaration(node, "global", getResource("field-public.png"));
+          break;
+
+        case "expr_stmt":
+          checkExpr_stmt(node, "var", getResource("field-public.png"));
+          break;
+
+        default:
+          node.getChildren().forEach(child -> child.accept(this));
+      }
     }
 
-    private List<Completion> allCompletions(String contents, AST root) {
-        AllCompletions completionVisitor = new AllCompletions(contents);
-        root.accept(completionVisitor);
-        return completionVisitor.getCompletions();
+    @Override
+    public void visit(Terminal token) {}
+
+    private void structureDeclaration(NonTerminal node, String name, URL url) {
+      Terminal structureIdent =
+          (Terminal)
+              node.getChildren()
+                  .stream()
+                  .filter(ast -> ast instanceof Terminal)
+                  .reduce((previous, current) -> current)
+                  .get();
+      completions.add(new Completion(name, structureIdent.extract(content), url));
+      node.getChildren().forEach(child -> child.accept(this));
     }
 
-    private class AllCompletions implements ASTVisitor {
+    private void checkExpr_stmt(NonTerminal node, String name, URL url) {
 
-        private TreeSet<String> variableNamesAlreadyOutlined = new TreeSet<String>();
-        private List<Completion> completions = new ArrayList<>();
-        private String content;
+      TerminalFinder finder = new TerminalFinder();
 
-        public AllCompletions(String content) {
-            this.content = content;
-        }
+      node.getChildren()
+          .stream()
+          .filter(ast -> ast instanceof NonTerminal)
+          .forEach(ident -> ident.accept(finder));
 
-        @Override
-        public void visit(NonTerminal node) {
+      String caption1stTerminalChild;
+      if (finder.getFoundTerminal() != null) {
+        caption1stTerminalChild = finder.getFoundTerminal().extract(content);
 
-            switch (node.getName()) {
+        node.getChildren()
+            .stream()
+            .filter(ast -> ast instanceof Terminal)
+            .limit(2)
+            .reduce((previous, current) -> current)
+            .ifPresent(
+                ident -> {
+                  String secondChild = ident.extract(content);
 
-                case "funcdef":
-                    addFuncToConverted(node, "def", getResource("method-public.png"));
-                    break;
-
-                case "classdef":
-                    classToConverted(node, "class", getResource("class-public.png"));
-                    break;
-
-                case "global_stmt":
-                    structureDeclaration(node, "global", getResource("field-public.png"));
-                    break;
-
-                case "expr_stmt":
-                    checkExpr_stmt(node, "var", getResource("field-public.png"));
-                    break;
-
-                default:
-                    node.getChildren().forEach(child -> child.accept(this));
-            }
-        }
-
-        @Override
-        public void visit(Terminal token) {
-
-        }
-
-        private void structureDeclaration(NonTerminal node, String name, URL url) {
-            Terminal structureIdent = (Terminal) node
-                    .getChildren()
-                    .stream()
-                    .filter(ast -> ast instanceof Terminal)
-                    .reduce((previous, current) -> current).get();
-            completions.add(new Completion(name, structureIdent.extract(content), url));
-            node.getChildren().forEach(child -> child.accept(this));
-        }
-
-        private void checkExpr_stmt(NonTerminal node, String name, URL url) {
-
-            TerminalFinder finder = new TerminalFinder();
-
-            node.getChildren()
-                    .stream()
-                    .filter(ast -> ast instanceof NonTerminal)
-                    .forEach(ident -> ident.accept(finder));
-
-
-            String caption1stTerminalChild;
-            if (finder.getFoundTerminal() != null) {
-                caption1stTerminalChild = finder.getFoundTerminal().extract(content);
-
-                node.getChildren()
-                        .stream()
-                        .filter(ast -> ast instanceof Terminal)
-                        .limit(2).reduce((previous, current) -> current)
-                        .ifPresent(ident -> {
-                            String secondChild = ident.extract(content);
-
-                            if (!variableNamesAlreadyOutlined.contains(caption1stTerminalChild) && secondChild.equals("=")) {
-                                completions.add(new Completion(name, caption1stTerminalChild, url));
-                                variableNamesAlreadyOutlined.add(caption1stTerminalChild);
-                            }
-                        });
-            }
-            node.getChildren().forEach(child -> child.accept(this));
-        }
-
-        private void addFuncToConverted(NonTerminal node, String name, URL url) {
-            Object[] terminalChildren = node.getChildren()
-                    .stream()
-                    .filter(ast -> ast instanceof Terminal)
-                    .toArray();
-            if (terminalChildren.length > 1) {
-                Terminal structureIdent = (Terminal) terminalChildren[1];
-                completions.add(new Completion(name, structureIdent.extract(content), url));
-
-            }
-            node.getChildren().forEach(child -> child.accept(this));
-        }
-
-        private void classToConverted(NonTerminal node, String name, URL url) {
-            Object[] terminalChildren = node.getChildren()
-                    .stream()
-                    .filter(ast -> ast instanceof Terminal)
-                    .toArray();
-            if (terminalChildren.length > 1) {
-                Terminal structureIdent = (Terminal) terminalChildren[0];
-                completions.add(new Completion(name, structureIdent.extract(content), url));
-
-            }
-            node.getChildren().forEach(child -> child.accept(this));
-        }
-
-
-        public List<Completion> getCompletions() {
-            return completions;
-        }
-
-        private class TerminalFinder implements ASTVisitor {
-
-            private Terminal foundTerminal = null;
-
-
-            @Override
-            public void visit(NonTerminal node) {
-                if (foundTerminal == null) {
-                    node.getChildren()
-                            .stream().forEach(child -> child.accept(this));
-                }
-            }
-
-            @Override
-            public void visit(Terminal token) {
-                foundTerminal = token;
-            }
-
-            public Terminal getFoundTerminal() {
-                return foundTerminal;
-            }
-        }
+                  if (!variableNamesAlreadyOutlined.contains(caption1stTerminalChild)
+                      && secondChild.equals("=")) {
+                    completions.add(new Completion(name, caption1stTerminalChild, url));
+                    variableNamesAlreadyOutlined.add(caption1stTerminalChild);
+                  }
+                });
+      }
+      node.getChildren().forEach(child -> child.accept(this));
     }
+
+    private void addFuncToConverted(NonTerminal node, String name, URL url) {
+      Object[] terminalChildren =
+          node.getChildren().stream().filter(ast -> ast instanceof Terminal).toArray();
+      if (terminalChildren.length > 1) {
+        Terminal structureIdent = (Terminal) terminalChildren[1];
+        completions.add(new Completion(name, structureIdent.extract(content), url));
+      }
+      node.getChildren().forEach(child -> child.accept(this));
+    }
+
+    private void classToConverted(NonTerminal node, String name, URL url) {
+      Object[] terminalChildren =
+          node.getChildren().stream().filter(ast -> ast instanceof Terminal).toArray();
+      if (terminalChildren.length > 1) {
+        Terminal structureIdent = (Terminal) terminalChildren[0];
+        completions.add(new Completion(name, structureIdent.extract(content), url));
+      }
+      node.getChildren().forEach(child -> child.accept(this));
+    }
+
+    public List<Completion> getCompletions() {
+      return completions;
+    }
+
+    private class TerminalFinder implements ASTVisitor {
+
+      private Terminal foundTerminal = null;
+
+      @Override
+      public void visit(NonTerminal node) {
+        if (foundTerminal == null) {
+          node.getChildren().stream().forEach(child -> child.accept(this));
+        }
+      }
+
+      @Override
+      public void visit(Terminal token) {
+        foundTerminal = token;
+      }
+
+      public Terminal getFoundTerminal() {
+        return foundTerminal;
+      }
+    }
+  }
 }
--- src/main/java/monto/service/python/PythonOutliner.java
+++ src/main/java/monto/service/python/PythonOutliner.java
@@ -1,5 +1,9 @@
 package monto.service.python;
 
+import java.net.URL;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.TreeSet;
 import monto.service.MontoService;
 import monto.service.ZMQConfiguration;
 import monto.service.ast.AST;
@@ -16,182 +20,173 @@ import monto.service.request.Request;
 import monto.service.source.SourceMessage;
 import monto.service.types.Languages;
 
-import java.net.URL;
-import java.util.ArrayDeque;
-import java.util.Deque;
-import java.util.TreeSet;
-
 public class PythonOutliner extends MontoService {
 
-    public PythonOutliner(ZMQConfiguration zmqConfig) {
-        super(zmqConfig,
-                PythonServices.PYTHON_OUTLINER,
-                "Outline",
-                "An outline service for Python",
-                Languages.PYTHON,
-                Products.OUTLINE,
-                options(),
-                dependencies(
-                        new SourceDependency(Languages.PYTHON),
-                        new ProductDependency(PythonServices.PYTHON_PARSER, Products.AST, Languages.PYTHON)
-                ));
+  public PythonOutliner(ZMQConfiguration zmqConfig) {
+    super(
+        zmqConfig,
+        PythonServices.PYTHON_OUTLINER,
+        "Outline",
+        "An outline service for Python",
+        Languages.PYTHON,
+        Products.OUTLINE,
+        options(),
+        dependencies(
+            new SourceDependency(Languages.PYTHON),
+            new ProductDependency(PythonServices.PYTHON_PARSER, Products.AST, Languages.PYTHON)));
+  }
+
+  @Override
+  public void onRequest(Request request) throws Exception {
+    SourceMessage version =
+        request
+            .getSourceMessage()
+            .orElseThrow(() -> new IllegalArgumentException("No version message in request"));
+    ProductMessage ast =
+        request
+            .getProductMessage(Products.AST, Languages.PYTHON)
+            .orElseThrow(() -> new IllegalArgumentException("No AST message in request"));
+
+    NonTerminal root = (NonTerminal) GsonMonto.fromJson(ast, AST.class);
+
+    OutlineTrimmer trimmer = new OutlineTrimmer(version.getContents());
+    root.accept(trimmer);
+
+    sendProductMessage(
+        version.getId(),
+        version.getSource(),
+        Products.OUTLINE,
+        Languages.PYTHON,
+        GsonMonto.toJsonTree(trimmer.getConverted()));
+  }
+
+  /**
+   * Traverses the AST and removes unneeded information.
+   */
+  private class OutlineTrimmer implements ASTVisitor {
+
+    private Deque<Outline> converted = new ArrayDeque<>();
+    private TreeSet<String> variableNamesAlreadyOutlined = new TreeSet<String>();
+    private final String content;
+
+    public Outline getConverted() {
+      return converted.getFirst();
+    }
+
+    public OutlineTrimmer(String content) {
+      this.content = content;
     }
 
     @Override
-    public void onRequest(Request request) throws Exception {
-        SourceMessage version = request.getSourceMessage()
-                .orElseThrow(() -> new IllegalArgumentException("No version message in request"));
-        ProductMessage ast = request.getProductMessage(Products.AST, Languages.PYTHON)
-                .orElseThrow(() -> new IllegalArgumentException("No AST message in request"));
+    public void visit(NonTerminal node) {
 
-        NonTerminal root = (NonTerminal) GsonMonto.fromJson(ast, AST.class);
+      switch (node.getName()) {
+        case "file_input":
+          converted.push(new Outline("file_input", node, null));
+          node.getChildren().forEach(child -> child.accept(this));
+          break;
 
-        OutlineTrimmer trimmer = new OutlineTrimmer(version.getContents());
-        root.accept(trimmer);
+        case "funcdef":
+          structureDeclaration(node, "function", getResource("method-public.png"));
+          break;
 
-        sendProductMessage(
-                version.getId(),
-                version.getSource(),
-                Products.OUTLINE,
-                Languages.PYTHON,
-                GsonMonto.toJsonTree(trimmer.getConverted()));
+        case "classdef":
+          structureDeclaration(node, "class", getResource("class-public.png"));
+          break;
+
+        case "global_stmt":
+          structureDeclaration(node, "global", getResource("field-public.png"));
+          break;
+
+        case "expr_stmt":
+          checkExpr_stmt(node, "var", getResource("field-public.png"));
+          break;
+
+        default:
+          node.getChildren().forEach(child -> child.accept(this));
+      }
     }
 
-    /**
-     * Traverses the AST and removes unneeded information.
-     */
-    private class OutlineTrimmer implements ASTVisitor {
+    @Override
+    public void visit(Terminal token) {}
 
-        private Deque<Outline> converted = new ArrayDeque<>();
-        private TreeSet<String> variableNamesAlreadyOutlined = new TreeSet<String>();
-        private final String content;
-
-        public Outline getConverted() {
-            return converted.getFirst();
-        }
-
-        public OutlineTrimmer(String content) {
-            this.content = content;
-        }
-
-        @Override
-        public void visit(NonTerminal node) {
-
-            switch (node.getName()) {
-
-                case "file_input":
-                    converted.push(new Outline("file_input", node, null));
-                    node.getChildren().forEach(child -> child.accept(this));
-                    break;
-
-                case "funcdef":
-                    structureDeclaration(node, "function", getResource("method-public.png"));
-                    break;
-
-                case "classdef":
-                    structureDeclaration(node, "class", getResource("class-public.png"));
-                    break;
-
-                case "global_stmt":
-                    structureDeclaration(node, "global", getResource("field-public.png"));
-                    break;
-
-                case "expr_stmt":
-                    checkExpr_stmt(node, "var", getResource("field-public.png"));
-                    break;
-
-                default:
-                    node.getChildren().forEach(child -> child.accept(this));
-            }
-        }
-
-        @Override
-        public void visit(Terminal token) {
-
-        }
-
-        private void structureDeclaration(NonTerminal node, String name, URL url) {
-            node.getChildren()
-                    .stream()
-                    .filter(ast -> ast instanceof Terminal)
-                    .limit(2)
-                    .reduce((previous, current) -> current)
-                    .ifPresent(ident -> {
-                        Outline structure = new Outline(name, ident, url);
-                        converted.peek().addChild(structure);
-                        converted.push(structure);
-                        node.getChildren().forEach(child -> child.accept(this));
-                        converted.pop();
-                    });
-        }
-
-        private void checkExpr_stmt(NonTerminal node, String name, URL url) {
-
-            TerminalFinder finder = new TerminalFinder();
-
-            node.getChildren()
-                    .stream()
-                    .filter(ast -> ast instanceof NonTerminal)
-                    .forEach(ident -> ident.accept(finder));
-
-
-            String caption1stTerminalChild;
-            if (finder.getFoundTerminal() != null) {
-                caption1stTerminalChild = extract(content, finder.getFoundTerminal()).toString();
-
-                node.getChildren()
-                        .stream()
-                        .filter(ast -> ast instanceof Terminal)
-                        .limit(2).reduce((previous, current) -> current)
-                        .ifPresent(ident -> {
-                            String secondChild = extract(content, ident).toString();
-
-                            if (!variableNamesAlreadyOutlined.contains(caption1stTerminalChild) && secondChild.equals("=")) {
-
-                                NonTerminal reducedNode = new NonTerminal(secondChild, node.getChildren().get(0));
-                                Outline variable = new Outline(name, reducedNode, url);
-                                converted.peek().addChild(variable);
-                                converted.push(variable);
-                                converted.pop();
-                                variableNamesAlreadyOutlined.add(caption1stTerminalChild);
-                            }
-                        });
-            }
-
-
-        }
-
-        private String extract(String str, AST indent) {
-            return str.subSequence(indent.getStartOffset(), indent.getStartOffset() + indent.getLength()).toString();
-        }
-
-        private class TerminalFinder implements ASTVisitor {
-
-            private Terminal foundTerminal = null;
-
-
-            @Override
-            public void visit(NonTerminal node) {
-                if (foundTerminal == null) {
-                    node.getChildren()
-                            .stream().forEach(child -> child.accept(this));
-                }
-
-            }
-
-            @Override
-            public void visit(Terminal token) {
-                foundTerminal = token;
-
-            }
-
-            public Terminal getFoundTerminal() {
-                return foundTerminal;
-            }
-
-        }
-
+    private void structureDeclaration(NonTerminal node, String name, URL url) {
+      node.getChildren()
+          .stream()
+          .filter(ast -> ast instanceof Terminal)
+          .limit(2)
+          .reduce((previous, current) -> current)
+          .ifPresent(
+              ident -> {
+                Outline structure = new Outline(name, ident, url);
+                converted.peek().addChild(structure);
+                converted.push(structure);
+                node.getChildren().forEach(child -> child.accept(this));
+                converted.pop();
+              });
     }
 
+    private void checkExpr_stmt(NonTerminal node, String name, URL url) {
 
+      TerminalFinder finder = new TerminalFinder();
+
+      node.getChildren()
+          .stream()
+          .filter(ast -> ast instanceof NonTerminal)
+          .forEach(ident -> ident.accept(finder));
+
+      String caption1stTerminalChild;
+      if (finder.getFoundTerminal() != null) {
+        caption1stTerminalChild = extract(content, finder.getFoundTerminal()).toString();
+
+        node.getChildren()
+            .stream()
+            .filter(ast -> ast instanceof Terminal)
+            .limit(2)
+            .reduce((previous, current) -> current)
+            .ifPresent(
+                ident -> {
+                  String secondChild = extract(content, ident).toString();
+
+                  if (!variableNamesAlreadyOutlined.contains(caption1stTerminalChild)
+                      && secondChild.equals("=")) {
+
+                    NonTerminal reducedNode =
+                        new NonTerminal(secondChild, node.getChildren().get(0));
+                    Outline variable = new Outline(name, reducedNode, url);
+                    converted.peek().addChild(variable);
+                    converted.push(variable);
+                    converted.pop();
+                    variableNamesAlreadyOutlined.add(caption1stTerminalChild);
+                  }
+                });
+      }
+    }
+
+    private String extract(String str, AST indent) {
+      return str.subSequence(indent.getStartOffset(), indent.getStartOffset() + indent.getLength())
+          .toString();
+    }
+
+    private class TerminalFinder implements ASTVisitor {
+
+      private Terminal foundTerminal = null;
+
+      @Override
+      public void visit(NonTerminal node) {
+        if (foundTerminal == null) {
+          node.getChildren().stream().forEach(child -> child.accept(this));
+        }
+      }
+
+      @Override
+      public void visit(Terminal token) {
+        foundTerminal = token;
+      }
+
+      public Terminal getFoundTerminal() {
+        return foundTerminal;
+      }
+    }
+  }
 }
